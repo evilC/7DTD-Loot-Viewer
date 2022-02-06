b1a407c81fdb5d7ae94eec8c41d73544b12be064
Clive Galway <clive.galway@ecs-digital.com>
2022-02-06 16:04:45 +0000

    Detect selection of an item

--- LootViewer/ViewModels/ItemListViewModel.cs
+++ LootViewer/ViewModels/ItemListViewModel.cs
@@ -1,7 +1,9 @@
-﻿using LootViewer.Models;
+﻿using Avalonia.Controls.Selection;
+using LootViewer.Models;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +12,25 @@ namespace LootViewer.ViewModels
 {
     public class ItemListViewModel : ViewModelBase
     {
+        public ObservableCollection<Item> Items { get; }
+        public SelectionModel<Item> Selection { get; }
+
         public ItemListViewModel(IEnumerable<Item> items)
         {
             Items = new ObservableCollection<Item>(items);
+            Selection = new SelectionModel<Item>();
+            Selection.SelectionChanged += SelectionChanged;
         }
 
-        public ObservableCollection<Item> Items { get; }
+        private void SelectionChanged(object? sender, SelectionModelSelectionChangedEventArgs<Item> e)
+        {
+            var foo = Selection.SelectedItem;
+            if (foo == null)
+            {
+                Debug.WriteLine($"Nothing selected");
+                return;
+            }
+            Debug.WriteLine($"Selected {foo.Description}");
+        }
     }
 }
