c20dfe358a4f008aa5153cb6ea9e6ad64decbade
Clive Galway <clive.galway@ecs-digital.com>
2022-02-06 20:08:32 +0000

    container list using actual data from database (static for now)

--- LootViewer/Services/Database.cs
+++ LootViewer/Services/Database.cs
@@ -11,33 +11,33 @@ namespace LootViewer.Services
 {
     public class Database
     {
-        //private SortedDictionary<string, Item> _items;
         private List<Models.Item> _items = new List<Models.Item>();
         private List<Container> _containers = new List<Container>();
+        private ItemContainerFinder rw;
 
         public Database(string lootXmlPath)
         {
             var loot = new LootParser(lootXmlPath);
+            rw = new ItemContainerFinder(loot.Data);
             //_items = loot.Data.Items;
             foreach (var item in loot.Data.Items)
             {
                 _items.Add(new Models.Item { Description = item.Key });
             }
 
+            _containers = new List<Container>();
+            var results = rw.GetItemContainers("meleeToolSalvageT2Ratchet");
+            foreach (var container in results.ContainerResults)
+            {
+                var cr = container.Value;
+                var probCalc = new ProbabilityCalculator(cr);
+                var prob = probCalc.CalculateProbability(102);
+                _containers.Add(new Container() { Name = container.Key, Prob = prob });
+            }
         }
-
-        //public IEnumerable<TodoItem> GetItems() => new List<TodoItem>()
-        //{
-        //    new TodoItem { Description = "Walk the dog" },
-        //    new TodoItem { Description = "Buy some milk" },
-        //    new TodoItem { Description = "Learn Avalonia", IsChecked = true },
-        //};
 
         public IEnumerable<Models.Item> GetItems() => _items;
 
-        public IEnumerable<Container> GetContainers() => new List<Container>() {
-            new Container() { Name = "C1", Prob = 1.23M },
-            new Container() { Name = "C2", Prob = 2.34M }
-        };
+        public IEnumerable<Container> GetContainers() => _containers;
     }
 }
